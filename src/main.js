c27a1e8a9f30345911762d9cb3cf8542f25b205a
Thiago Marinho <tgmarinho@gmail.com>
2017-12-15 12:07:05 -0300

    exmplos feito no curso js-tdd-ws

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 const a = 2;
-const b = 4
+const b = 4;
 
-const c = 'husaidha';
+const c = 'lol';
 
 function sum() {
   return a + b + c;
+}
+
+const nome  = "silva";
+  console.log(nome);
+
+{
+  const nome  = "jorge";
+    console.log(nome);
+}
+
+{
+console.log("aff")
+
 }
 
 sum();
