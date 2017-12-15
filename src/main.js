76dc48b3b818a8b8473576915295ed5b99e835eb
Thiago Marinho <tgmarinho@gmail.com>
2017-12-15 12:08:16 -0300

    resolve eslint e guide style

--- src/main.js
+++ src/main.js
@@ -6,18 +6,3 @@ const c = 'lol';
 function sum() {
   return a + b + c;
 }
-
-const nome  = "silva";
-  console.log(nome);
-
-{
-  const nome  = "jorge";
-    console.log(nome);
-}
-
-{
-console.log("aff")
-
-}
-
-sum();
