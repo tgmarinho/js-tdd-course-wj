7acf29a42f8531bf7b1eac6d45f46f2d7dfcc9b5
= <tgmarinho@gmail.com>
2018-01-05 10:58:46 -0300

    tudo em ES agora

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import calc from '../src/main';
+import { sum, sub, mult, div } from '../src/main';
 
 describe('Calc', () => {
 
@@ -12,29 +12,24 @@ describe('Calc', () => {
 
   describe('Smoke tests', () => {
 
-    it('should exist the calc lib', () => {
-      expect(calc).to.exist;
-
-    });
-
-    it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist;
-      expect(calc.sum).to.be.a('function');
+     it('should exist the method `sum`', () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a('function');
     });
 
     it('should exist the method `div`', () => {
-      expect(calc.div).to.exist;
-      expect(calc.div).to.be.a('function');
+      expect(div).to.exist;
+      expect(div).to.be.a('function');
     });
 
     it('should exist the method `mult`', () => {
-      expect(calc.mult).to.exist;
-      expect(calc.mult).to.be.a('function');
+      expect(mult).to.exist;
+      expect(mult).to.be.a('function');
     });
 
     it('should exist the method `sub`', () => {
-      expect(calc.sub).to.exist;
-      expect(calc.sub).to.be.a('function');
+      expect(sub).to.exist;
+      expect(sub).to.be.a('function');
     });
 
   });
